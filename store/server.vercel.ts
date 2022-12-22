63203caa2a0c996a780b96bf155467ebb37d6b96
lychee <lychee@rb2.nl>
2022-12-22 17:32:02 +0800

    trigger cicd

--- store/server.vercel.ts
+++ store/server.vercel.ts
@@ -1,21 +1,5 @@
-import type { AppLoadContext, ServerBuild } from "@remix-run/server-runtime";
-import type { GetLoadContextFunction } from "@remix-run/vercel";
-import { createRequestHandler } from "@remix-run/vercel";
-import { createRemixRequest } from "@remix-run/vercel/dist/server";
+import * as build from "@remix-run/dev/server-build";
+import { getContext } from "./server/context";
+import { vercelServer } from "./server/vercel";
 
-interface VercelServerParams {
-  getContext: (request: Request) => AppLoadContext;
-  build: ServerBuild;
-}
-
-export const vercelServer = ({ getContext, build }: VercelServerParams) => {
-  const getLoadContext: GetLoadContextFunction = (req, res) => {
-    const request = createRemixRequest(req, res);
-    return getContext(request);
-  };
-  return createRequestHandler({
-    build,
-    mode: process.env.NODE_ENV,
-    getLoadContext,
-  });
-};
+export default vercelServer({ getContext, build });
