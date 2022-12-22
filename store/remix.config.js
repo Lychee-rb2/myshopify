fae0076c9edfcf78e4732888f19a2f46589907f8
lychee <lychee@rb2.nl>
2022-12-22 17:26:45 +0800

    trigger cicd

--- store/remix.config.js
+++ store/remix.config.js
@@ -1,5 +1,14 @@
+const serverBuildTarget =
+  process.env.NODE_ENV === "development"
+    ? undefined
+    : process.env.SERVER_TARGET_NODE === "true"
+      ? undefined
+      : "vercel";
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  serverBuildTarget,
+  server: serverBuildTarget === "vercel" ? "./server.vercel.js" : undefined,
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
