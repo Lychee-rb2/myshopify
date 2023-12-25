d116a0f70cf5a2f0f50a0c6084691276d2bed45a
lychee <lychee@rb2.nl>
2023-12-25 12:27:30 +0800

    feat: Setup sst

--- backend/packages/functions/src/hello.ts
+++ backend/packages/functions/src/hello.ts
@@ -1,7 +1,13 @@
-export const handler = () => {
+import { APIGatewayProxyHandlerV2 } from 'aws-lambda'
+
+export const handler: APIGatewayProxyHandlerV2 = async () => {
   console.log('Hello world')
   return {
     statusCode: 200,
+    isBase64Encoded: false,
+    headers: {
+      "Content-Type": "application/json",
+    },
     body: JSON.stringify({ message: "Hello world" }),
   };
 }
